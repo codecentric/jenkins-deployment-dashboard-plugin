a91b22f22c0457e95eb2cc152e5ca3b0822d6d26
Miel Donkers <miel.donkers@codecentric.nl>
2014-08-01 14:38:01 +0200

    Added first configuration properties

--- src/main/java/de/codecentric/jenkins/dashboard/DashboardView.java
+++ src/main/java/de/codecentric/jenkins/dashboard/DashboardView.java
@@ -1,5 +1,7 @@
 package de.codecentric.jenkins.dashboard;
 
+import static de.codecentric.jenkins.dashboard.util.LocalMessages.DASHBOARD_VIEW_DISPLAYNAME;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -7,6 +9,7 @@ import java.util.Collection;
 import javax.servlet.ServletException;
 
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
@@ -18,24 +21,34 @@ import hudson.model.TopLevelItem;
 import hudson.model.View;
 import hudson.model.ViewDescriptor;
 import hudson.model.ViewGroup;
+import hudson.util.FormValidation;
 import jenkins.model.Jenkins;
 
-/**
- * @author Miel Donkers (miel.donkers@codecentric.nl)
- */
 public class DashboardView extends View {
 
     @Extension
     public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
+
+    private String artifactoryRestUri = "";
+    private String username = "";
+    private String password = "";
 
 
     public DashboardView(final String name) {
         super(name);
     }
 
-    @DataBoundConstructor
     public DashboardView(final String name, final ViewGroup owner) {
         super(name, owner);
+    }
+
+    @DataBoundConstructor
+    public DashboardView(final String name, final String artifactoryRestUri, final String username,
+            final String password) {
+        this(name);
+        setArtifactoryRestUri(artifactoryRestUri);
+        setUsername(username);
+        setPassword(password);
     }
 
     @Override
@@ -86,17 +99,96 @@ public class DashboardView extends View {
         return Jenkins.getInstance().doCreateItem(req, rsp);
     }
 
+    public String getArtifactoryRestUri() {
+        return artifactoryRestUri;
+    }
+
+    public void setArtifactoryRestUri(final String artifactoryRestUri) {
+        this.artifactoryRestUri = artifactoryRestUri;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(final String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+
+
     public static class DescriptorImpl extends ViewDescriptor {
 
-        /**
-         * Returns the human-readable name of this type of view. Used
-         * in the view creation screen. The string should look like
-         * "Abc Def Ghi".
-         */
         @Override
         public String getDisplayName() {
-            return "DashboardView";
+            return DASHBOARD_VIEW_DISPLAYNAME.toString();
         }
+
+        @Override
+        public String getHelpFile() {
+            return "/plugin/jenkins-deployment-dashboard-plugin/help.html";
+        }
+
+        public FormValidation doCheckArtifactoryRestUri(@QueryParameter final String artifactoryRestUri) {
+            FormValidation validationResult;
+
+//            if (RestConnection.validateRestUri(artifactoryRestUri)) {
+                validationResult = FormValidation.ok();
+//            } else {
+//                validationResult = FormValidation.error("Artifactory REST uri is not valid, cannot be empty and has to " +
+//                        "start with 'http://' or 'https://'");
+//            }
+
+            return validationResult;
+        }
+
+        public FormValidation doCheckUsername(@QueryParameter final String username) {
+            FormValidation validationResult;
+
+//            if (RestConnection.validateUsername(username)) {
+                validationResult = FormValidation.ok();
+//            } else {
+//                validationResult = FormValidation.error("Username for REST interface cannot be empty");
+//            }
+
+            return validationResult;
+        }
+
+        public FormValidation doCheckPassword(@QueryParameter final String password) {
+            FormValidation validationResult;
+
+//            if (RestConnection.validatePassword(password)) {
+                validationResult = FormValidation.ok();
+//            } else {
+//                validationResult = FormValidation.error("Password for REST interface cannot be empty");
+//            }
+
+            return validationResult;
+        }
+
+        public FormValidation doTestArtifactoryConnection(@QueryParameter("artifactoryRestUri") final String artifactoryRestUri,
+                @QueryParameter("username") final String username,
+                @QueryParameter("password") final String password) {
+            FormValidation validationResult;
+//            RestConnection connection = new RestConnection(artifactoryRestUri, username, password, applicationName);
+
+//            if (connection.validateConnection()) {
+                validationResult = FormValidation.ok("Connection successful");
+//            } else {
+//                validationResult = FormValidation.warning("Connection with Artifactory RESTful interface could not be established");
+//            }
+
+            return validationResult;
+        }
+
+
     }
 
 }
