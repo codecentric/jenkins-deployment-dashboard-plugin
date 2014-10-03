34e14144f389028505a9cf6da0f4e8bf2f927892
Miel Donkers <miel.donkers@codecentric.nl>
2014-10-03 22:44:53 +0200

    Extracted DashboardViewDescriptor to separate class

--- src/main/java/de/codecentric/jenkins/dashboard/DashboardView.java
+++ src/main/java/de/codecentric/jenkins/dashboard/DashboardView.java
@@ -1,19 +1,5 @@
 package de.codecentric.jenkins.dashboard;
 
-import hudson.Extension;
-import hudson.model.AbstractProject;
-import hudson.model.Cause;
-import hudson.model.Item;
-import hudson.model.ParametersAction;
-import hudson.model.StringParameterValue;
-import hudson.model.TopLevelItem;
-import hudson.model.ViewGroup;
-import hudson.model.Descriptor;
-import hudson.model.ModifiableItemGroup;
-import hudson.model.View;
-import hudson.model.ViewDescriptor;
-import hudson.util.FormValidation;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -25,30 +11,36 @@ import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 
-import net.sf.json.JSONObject;
-
-import hudson.util.ListBoxModel;
-import jenkins.model.Jenkins;
-
 import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 import org.kohsuke.stapler.bind.JavaScriptMethod;
-import org.springframework.util.StringUtils;
+
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 
-import de.codecentric.jenkins.dashboard.Environment.EnvironmentDescriptor;
 import de.codecentric.jenkins.dashboard.api.environment.ServerEnvironment;
 import de.codecentric.jenkins.dashboard.api.repository.Artifact;
 import de.codecentric.jenkins.dashboard.api.repository.RepositoryInterface;
 import de.codecentric.jenkins.dashboard.artifactrepositories.ArtifactoryConnector;
 import de.codecentric.jenkins.dashboard.artifactrepositories.NexusConnector;
 import de.codecentric.jenkins.dashboard.ec2.EC2Connector;
+import hudson.Extension;
+import hudson.model.AbstractProject;
+import hudson.model.Cause;
+import hudson.model.Descriptor;
+import hudson.model.Item;
+import hudson.model.ModifiableItemGroup;
+import hudson.model.ParametersAction;
+import hudson.model.StringParameterValue;
+import hudson.model.TopLevelItem;
+import hudson.model.View;
+import hudson.model.ViewDescriptor;
+import hudson.model.ViewGroup;
+import jenkins.model.Jenkins;
 
 /**
  * Dashboard View
@@ -59,14 +51,12 @@ public class DashboardView extends View {
 	private final static Logger LOGGER = Logger.getLogger(DashboardView.class.getName());
 
 	@Extension
-	public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
+	public static final DashboardViewDescriptor DESCRIPTOR = new DashboardViewDescriptor();
 
 	private boolean showDeployField;
 
 	private String groupId = "";
 	private String artefactId = "";
-	
-	private String errorMessage = "";
 	
 	private List<Environment> environments;
 	
@@ -234,9 +224,7 @@ public class DashboardView extends View {
 		RepositoryInterface repository;
 		
 		System.out.println(DESCRIPTOR.getRepositoryType());
-		
-		
-		
+
 		if( DESCRIPTOR.getRepositoryType().equalsIgnoreCase( RepositoryType.ARTIFACTORY.getid() )) {
 			repository = new ArtifactoryConnector(DESCRIPTOR.getUsername(), DESCRIPTOR.getPassword(), repositoryURI);
 		} else {
@@ -284,183 +272,4 @@ public class DashboardView extends View {
 				: new ArrayList<Environment>(environmentsList);
 	}
 
-    public static enum AwsRegion {
-        AP_NORTHEAST_1("ap-northeast-1", "Asia Pacific (Tokyo) Region"),
-        AP_SOUTHEAST_1("ap-southeast-1", "Asia Pacific (Singapore) Region"),
-        AP_SOUTHEAST_2("ap-southeast-2", "Asia Pacific (Sydney) Region"),
-        EU_WEST_1("eu-west-1", "EU (Ireland) Region"),
-        SA_EAST_1("sa-east-1", "South America (Sao Paulo) Region"),
-        US_EAST_1("us-east-1", "US East (Northern Virginia) Region"),
-        US_WEST_1("us-west-1", "US West (Northern California) Region"),
-        US_WEST_2("us-west-2", "US West (Oregon) Region");
-
-        private final String identifier;
-        private final String name;
-
-        private AwsRegion(final String identifier, final String name) {
-            this.identifier = identifier;
-            this.name = name;
-        }
-    }
-
-    public static final class DescriptorImpl extends ViewDescriptor {
-
-        private String repositoryType;
-        private String repositoryRestUri = "";
-        private String username = "";
-        private String password = "";
-
-        private String awsAccessKey = "";
-        private String awsSecretKey = "";
-        private String awsRegion = "";
-
-        public DescriptorImpl() {
-            super();
-            load();
-        }
-
-		@Override
-		public String getDisplayName() {
-			return Messages.DashboardView_DisplayName();
-		}
-
-		public List<EnvironmentDescriptor> getEnvironmentDescriptors() {
-			return Jenkins.getInstance().getDescriptorList(Environment.class);
-		}
-
-		@Override
-		public String getHelpFile() {
-			return "/plugin/jenkins-deployment-dashboard-plugin/help.html";
-		}
-
-        public ListBoxModel doFillRepositoryTypeItems() {
-            final ListBoxModel model = new ListBoxModel();
-
-            for (RepositoryType value : RepositoryType.values()) {
-                model.add(value.getDescription(), value.getid());
-            }
-
-            return model;
-        }
-
-        public ListBoxModel doFillAwsRegionItems() {
-            final ListBoxModel model = new ListBoxModel();
-
-            for (AwsRegion value : AwsRegion.values()) {
-                model.add(value.name, value.identifier);
-            }
-
-            return model;
-        }
-
-        public FormValidation doCheckArtifactoryRestUri(@QueryParameter final String artifactoryRestUri) {
-			return FormValidation.ok();
-		}
-
-		public FormValidation doCheckUsername(@QueryParameter final String username) {
-			
-			if( StringUtils.hasText(username) ) {
-				return FormValidation.ok();
-			}
-			return FormValidation.warning(Messages.DashboardView_artifactoryUsername());
-		}
-
-		public FormValidation doCheckPassword(@QueryParameter final String password) {
-
-			if( StringUtils.hasText(password) ) {
-				return FormValidation.ok();
-			}
-			return FormValidation.warning(Messages.DashboardView_artifactoryPassword());
-		}
-
-		public FormValidation doTestRepositoryConnection(
-				@QueryParameter("repositoryRestUri") final String repositoryRestUri,
-				@QueryParameter("username") final String username,
-				@QueryParameter("password") final String password) {
-
-			LOGGER.info("Verify Repository connection for URI " + repositoryRestUri);
-
-			FormValidation validationResult;
-			try {
-				URI repositoryURI = new URI(repositoryRestUri);
-				ArtifactoryConnector repository = new ArtifactoryConnector(username, password, repositoryURI);
-				LOGGER.info("Artifactory config valid? " + repository.canConnect());
-				if (repository.canConnect()) {
-					validationResult = FormValidation.ok(Messages.DashboardView_artifactoryConnectionSuccessful());
-				} else {
-					validationResult = FormValidation.warning(Messages.DashboardView_artifactoryConnectionFailed() + repositoryRestUri);
-				}
-
-			} catch (Exception e) {
-				LOGGER.severe(e.getMessage());
-				validationResult = FormValidation.error(Messages.DashboardView_artifactoryConnectionCritical() + e.getMessage());
-			}
-
-			return validationResult;
-		}
-
-        @Override
-        public boolean configure(StaplerRequest req, JSONObject json) throws Descriptor.FormException {
-            req.bindJSON(this, json.getJSONObject("deployment-dashboard"));
-            save();
-            return true;
-        }
-
-        public String getRepositoryType() {
-            return repositoryType;
-        }
-
-        public void setRepositoryType(String repositoryType) {
-            this.repositoryType = repositoryType;
-        }
-
-        public String getRepositoryRestUri() {
-            return repositoryRestUri;
-        }
-
-        public void setRepositoryRestUri(final String repositoryRestUri) {
-            this.repositoryRestUri = repositoryRestUri;
-        }
-
-        public String getUsername() {
-            return username;
-        }
-
-        public void setUsername(final String username) {
-            this.username = username;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(final String password) {
-            this.password = password;
-        }
-
-        public String getAwsAccessKey() {
-            return awsAccessKey;
-        }
-
-        public void setAwsAccessKey(String awsAccessKey) {
-            this.awsAccessKey = awsAccessKey;
-        }
-
-        public String getAwsSecretKey() {
-            return awsSecretKey;
-        }
-
-        public void setAwsSecretKey(String awsSecretKey) {
-            this.awsSecretKey = awsSecretKey;
-        }
-
-        public String getAwsRegion() {
-            return awsRegion;
-        }
-
-        public void setAwsRegion(final String awsRegion) {
-            this.awsRegion = awsRegion;
-        }
-
-    }
 }
---
+++ src/main/java/de/codecentric/jenkins/dashboard/DashboardViewDescriptor.java
@@ -0,0 +1,182 @@
+package de.codecentric.jenkins.dashboard;
+
+import java.net.URI;
+import java.util.List;
+import java.util.logging.Logger;
+
+import net.sf.json.JSONObject;
+
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.StaplerRequest;
+import org.springframework.util.StringUtils;
+
+
+import de.codecentric.jenkins.dashboard.artifactrepositories.ArtifactoryConnector;
+import hudson.model.Descriptor;
+import hudson.model.ViewDescriptor;
+import hudson.util.FormValidation;
+import hudson.util.ListBoxModel;
+import jenkins.model.Jenkins;
+
+public final class DashboardViewDescriptor extends ViewDescriptor {
+
+    private final static Logger LOGGER = Logger.getLogger(DashboardViewDescriptor.class.getName());
+
+    private String repositoryType;
+    private String repositoryRestUri = "";
+    private String username = "";
+    private String password = "";
+
+    private String awsAccessKey = "";
+    private String awsSecretKey = "";
+    private String awsRegion = "";
+
+    public DashboardViewDescriptor() {
+        super(DashboardView.class); // Have to provide the original class because there is no enclosing class
+        load();
+    }
+
+    @Override
+    public String getDisplayName() {
+        return Messages.DashboardView_DisplayName();
+    }
+
+    public List<Environment.EnvironmentDescriptor> getEnvironmentDescriptors() {
+        return Jenkins.getInstance().getDescriptorList(Environment.class);
+    }
+
+    @Override
+    public String getHelpFile() {
+        return "/plugin/jenkins-deployment-dashboard-plugin/help.html";
+    }
+
+    public ListBoxModel doFillRepositoryTypeItems() {
+        final ListBoxModel model = new ListBoxModel();
+
+        for (RepositoryType value : RepositoryType.values()) {
+            model.add(value.getDescription(), value.getid());
+        }
+
+        return model;
+    }
+
+    public ListBoxModel doFillAwsRegionItems() {
+        final ListBoxModel model = new ListBoxModel();
+
+        for (AwsRegion value : AwsRegion.values()) {
+            model.add(value.getName(), value.getIdentifier());
+        }
+
+        return model;
+    }
+
+    public FormValidation doCheckArtifactoryRestUri(@QueryParameter final String artifactoryRestUri) {
+        return FormValidation.ok();
+    }
+
+    public FormValidation doCheckUsername(@QueryParameter final String username) {
+
+        if( StringUtils.hasText(username) ) {
+            return FormValidation.ok();
+        }
+        return FormValidation.warning(Messages.DashboardView_artifactoryUsername());
+    }
+
+    public FormValidation doCheckPassword(@QueryParameter final String password) {
+
+        if( StringUtils.hasText(password) ) {
+            return FormValidation.ok();
+        }
+        return FormValidation.warning(Messages.DashboardView_artifactoryPassword());
+    }
+
+    public FormValidation doTestRepositoryConnection(
+            @QueryParameter("repositoryRestUri") final String repositoryRestUri,
+            @QueryParameter("username") final String username,
+            @QueryParameter("password") final String password) {
+
+        LOGGER.info("Verify Repository connection for URI " + repositoryRestUri);
+
+        FormValidation validationResult;
+        try {
+            URI repositoryURI = new URI(repositoryRestUri);
+            ArtifactoryConnector repository = new ArtifactoryConnector(username, password, repositoryURI);
+            LOGGER.info("Artifactory config valid? " + repository.canConnect());
+            if (repository.canConnect()) {
+                validationResult = FormValidation.ok(Messages.DashboardView_artifactoryConnectionSuccessful());
+            } else {
+                validationResult = FormValidation.warning(Messages.DashboardView_artifactoryConnectionFailed() + repositoryRestUri);
+            }
+
+        } catch (Exception e) {
+            LOGGER.severe(e.getMessage());
+            validationResult = FormValidation.error(Messages.DashboardView_artifactoryConnectionCritical() + e.getMessage());
+        }
+
+        return validationResult;
+    }
+
+    @Override
+    public boolean configure(StaplerRequest req, JSONObject json) throws Descriptor.FormException {
+        req.bindJSON(this, json.getJSONObject("deployment-dashboard"));
+        save();
+        return true;
+    }
+
+    public String getRepositoryType() {
+        return repositoryType;
+    }
+
+    public void setRepositoryType(String repositoryType) {
+        this.repositoryType = repositoryType;
+    }
+
+    public String getRepositoryRestUri() {
+        return repositoryRestUri;
+    }
+
+    public void setRepositoryRestUri(final String repositoryRestUri) {
+        this.repositoryRestUri = repositoryRestUri;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(final String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+
+    public String getAwsAccessKey() {
+        return awsAccessKey;
+    }
+
+    public void setAwsAccessKey(String awsAccessKey) {
+        this.awsAccessKey = awsAccessKey;
+    }
+
+    public String getAwsSecretKey() {
+        return awsSecretKey;
+    }
+
+    public void setAwsSecretKey(String awsSecretKey) {
+        this.awsSecretKey = awsSecretKey;
+    }
+
+    public String getAwsRegion() {
+        return awsRegion;
+    }
+
+    public void setAwsRegion(final String awsRegion) {
+        this.awsRegion = awsRegion;
+    }
+
+}
